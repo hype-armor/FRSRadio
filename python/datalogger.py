43ae8d3d09055d450ac988365b9ea3863043087b
Greg Morgan <gamm90@gmail.com>
2017-06-08 19:26:13 -0500

    Update datalogger with error handling.

--- python/datalogger.py
+++ python/datalogger.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 import serial
 import serial.tools.list_ports
 import datetime
+import time
+from threading import Lock
+lock = Lock()
 
 def findArduino():
     ports = list(serial.tools.list_ports.comports())
@@ -9,27 +13,32 @@ def findArduino():
             print ("Using serial port: " + p[0])
             return str(p[0])
 
-time = datetime.datetime.now().time().isoformat()
-f = open(time + ".csv",'a')
+dt = datetime.datetime.now().time().isoformat()
+f = open(dt + ".csv",'a')
 
-while True:
-    try:
-        time = datetime.datetime.now().time().isoformat()
-        f.write(time + "," + ser.readline())
-        f.flush()
-    except:
-        time.sleep(0.1)
-        port = findArduino()
-        if port is not None:
-            # reconnect
-            try:
-                print ("Connecting...")
-                ser = serial.Serial(port, 115200)
-                time = datetime.datetime.now().time().isoformat()
-                f = open(time + ".csv",'a')
-            except:
+try:
+    while True:
+        try:
+            dt = datetime.datetime.now().time().isoformat()
+            f.write(dt + "," + ser.readline())
+            f.flush()
+        except:
+            time.sleep(1)
+            port = findArduino()
+            if port is not None:
+                # reconnect
+                try:
+                    print ("Connecting...")
+                    ser = serial.Serial(port, 115200)
+                    dt = datetime.datetime.now().time().isoformat()
+                    f = open(dt + ".csv",'a')
+                except:
+                    print ("Connect failed.")
+                    f.close()
+            else:
                 print ("Connect failed.")
                 f.close()
-        else:
-            print ("Connect failed.")
-            f.close()
+except KeyboardInterrupt:
+    print "attempting to close threads."
+    t.join(0)
+    print "threads successfully closed"
